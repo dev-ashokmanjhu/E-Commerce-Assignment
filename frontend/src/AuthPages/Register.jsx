364fe355ecd2004fa044995380efeec3fe82213d
dev-ashokmanjhu <dev.ashokmanjhu@gmail.com>
2023-04-06 10:25:58 +0530

    update loader

--- frontend/src/AuthPages/Register.jsx
+++ frontend/src/AuthPages/Register.jsx
@@ -131,7 +131,41 @@ const Register = () => {
           </div>
           <div>
             <button className="loginBtn" type="submit">
-              {isLoading ? "Register..." : "Register"}
+              {isLoading ? (
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    className="spinner_9y7u"
+                    x="1"
+                    y="1"
+                    rx="1"
+                    width="10"
+                    height="10"
+                  />
+                  <rect
+                    class="spinner_9y7u spinner_DF2s"
+                    x="1"
+                    y="1"
+                    rx="1"
+                    width="10"
+                    height="10"
+                  />
+                  <rect
+                    class="spinner_9y7u spinner_q27e"
+                    x="1"
+                    y="1"
+                    rx="1"
+                    width="10"
+                    height="10"
+                  />
+                </svg>
+              ) : (
+                "Register"
+              )}
             </button>
           </div>
           <div className="signUpText">
